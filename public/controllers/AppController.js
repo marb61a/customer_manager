542be1feacf84c700e8774c832132785c060ea18
marb61a <marb61a@gmail.com>
2016-03-18 14:25:56 +0000

    added add client function to controller

---
+++ public/controllers/AppController.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppController', ['$scope', '$http', '$location', function($scope, $http, $location){
+  $http.get('/clients').success(function(response){
+		console.log('Data recieved from the server');
+		$scope.clients = response;
+	});
+
+	$scope.addClient = function(){
+		console.log('Adding new client...');
+		$http.post('/clients', $scope.client).success(function(response){
+			console.log('Client Added');
+			window.location.href='/';
+		});
+	}
+  
+}])
