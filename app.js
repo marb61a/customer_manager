7a5ef9009e0ef834fd3a3753800eaf3a573d7909
marb61a <marb61a@gmail.com>
2016-03-18 14:17:04 +0000

    added get and post functions to app

--- app.js
+++ app.js
@@ -21,5 +21,30 @@ app.get('/', function(req, res){
 	});
 });
 
+app.post('/clients', function(){
+  console.log('Request for clients received...');
+
+	db.clients.find().sort({first_name: 1}, function(err, docs){
+		if(err){
+			res.send(err);
+		} else {
+			console.log('Sending Data...');
+			res.json(docs);
+		}
+	});
+});
+
+app.get('/clients', function(){
+  var id = req.params.id;
+
+	db.clients.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+		if(err){
+			res.send(err);
+		} else {
+			res.json(doc);
+		}
+	});
+});
+
 app.listen(3000);
 console.log('Ready on port 3000...');
